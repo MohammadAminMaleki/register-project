293fdb4cb187caedb180da1429475bd4ada8d2b7
mohammadamin <mhmdamyjl12@gmail.com>
2024-07-24 09:22:24 +0330

    some changes

--- js/app.js
+++ js/app.js
@@ -3,15 +3,22 @@ let validPassword = "123456"
 
 let userName = document.getElementById('username')
 let password = document.getElementById('password')
-let link = document.getElementById('link')
-console.log(link)
+let login = document.getElementById('login')
+let signup = document.getElementById('signup')
 
-link.addEventListener("click",function (event){
+login.addEventListener("click",function (event){
     event.preventDefault()
     if(userName.value == validUserName && password.value == validPassword){
         window.location.href='./sales-invoice.html'
     }else{
         alert('نام کاربری و رمز ورود نامعتبر است.')
-        link.setAttribute("target","#")
+        // login.setAttribute("target","#")
     }
 })
+
+signup.addEventListener('click',function (event){
+    event.preventDefault()
+    window.location.href=""
+})
+
+
